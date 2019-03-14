00b69a354f32c96ff7233677de98b41c1083bb66
Bruno Batista <bbnsdevelop@gmail.com>
2019-03-13 21:46:23 -0300

    final da sessão - configurando apollo client, configurando tratamento de erro

--- graphcool-chat/src/app/apollo-config.module.ts
+++ graphcool-chat/src/app/apollo-config.module.ts
@@ -3,7 +3,9 @@ import { NgModule } from '@angular/core';
 import { ApolloModule, Apollo } from 'apollo-angular';
 import {HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { environment } from 'src/environments/environment';
+import { environment } from '../environments/environment';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
 
 @NgModule({
   imports: [
@@ -17,8 +19,23 @@ export class ApolloConfigModule {
   constructor(private apollo: Apollo, private httpLink: HttpLink) {
     const uri = 'https://api.graph.cool/simple/v1/cjsj6ygs820w30149x9ron8ir';
     const http = this.httpLink.create({uri});
+
+    const linkError = onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors) {
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          ),
+        );
+        }
+      if (networkError) {console.log(`[Network error]: ${networkError}`);}
+    });
+
     this.apollo.create({
-      link: http,
+      link: ApolloLink.from([
+        linkError,
+        http
+      ]),
       cache: new InMemoryCache(),
       connectToDevTools: !environment.production
     });
